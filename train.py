f1b999b924c7f30186de5bf64c0e5e265dbe7b9d
zle1992 <625015879@qq.com>
2018-05-31 21:04:14 +0800

    modeify for py3

--- train.py
+++ train.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os
-os.environ["CUDA_VISIBLE_DEVICES"] = "0"
+#os.environ["CUDA_VISIBLE_DEVICES"] = "0"
 
 import tensorflow as tf
 tfconfig = tf.ConfigProto()
